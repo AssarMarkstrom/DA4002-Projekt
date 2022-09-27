f930b0b12d509dda6e6f1df5905aa0bc812b787a
Pia <pia.glimmerfors@gmail.com>
2022-09-27 11:13:45 +0200

    First draft

--- file_reader.py
+++ file_reader.py
@@ -4,14 +4,27 @@ Reading and saving files
 
 import pandas as pd
 
-def read_csv(file_path):
-    df = pd.read_csv(file_path)
-    print(df)
+def file_read(file_path):
+
+    filetype = file_path[-3:]
+
+    if filetype == "csv":        
+        df = pd.read_csv(file_path)
+    elif filetype == "tsv":
+        df = pd.read_csv(file_path,sep='\t')
+    elif filetype == "xls" or file_path[-4:] == "xlsx":
+        df = pd.read_excel(file_path)        
+    else:
+        raise Exception("Invalid file format")
+
+    return df
+
+
+def detect_NAs(df):
+    print(df[df.isna().any(axis=1)])
+detect_NAs(file_read(r"C:\Users\piagl\Documents\Kurser\DA4002 - Mjukvaruutveckling\DA4002-Projekt\Testfiles\25_years_of_salgskimmer.csv"))
 
 
 
-read_csv(r"C:\Users\piagl\Documents\Kurser\DA4002 - Mjukvaruutveckling\DA4002-Projekt")
 
-# def read_tsv(file_path):
-        
-    
+
