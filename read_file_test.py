8828279318f0ccf75ce26b37a28e2c105727722e
Natalija Paunic <natalijapaunic@hotmail.com>
2022-09-27 18:50:37 +0200

    Small changes to comments

--- read_file_test.py
+++ read_file_test.py
@@ -1,48 +1,49 @@
 #!/bin/env python3
 import unittest
-import file_reader
+import fileread
 
 class ReadFileTestCase(unittest.TestCase):
     def setUp(self):
         self.csv_file = "testfile.csv"
         self.tsv_file = "testfile.tsv"
+        self.xls_file = "testfile.xls"
+        self.xlsx_file = "testfile.xlsx"
 
 # ID file tests
     # if the id is a csv file, it reads as CSV.
     def test_id_csv(self):
         self.assertEqual(
-                file_reader.id_file(self.csv_file),
+                fileread.id_file(self.csv_file),
                 "CSV",
                 "Incorrect file type")
 
     # if the id is a tsv file, it reads as TSV.
     def test_id_tsv(self):
         self.assertEqual(
-                file_reader.id_file(self.tsv_file), 
+                fileread.id_file(self.tsv_file), 
                 "TSV",
-                "Incorrect file type")
-
-    # if the id is an unknow file, it reads as UNKNOWN.
-    def test_id_unknown(self):
-        self.assertEqual(
-                file_reader.id_file("asdasda"), 
-                "UNKNOWN",
                 "Incorrect file type")
     
     # if the id isa xls file, it reads as XLS.
     def test_id_xls(self):
         self.assertEqual(
-                file_reader.id_file(self.xls_file),
+                fileread.id_file(self.xls_file),
                 "XLS",
                 "Incorrect file type")
 
     # if the file id is a xlsx file, it reads as XLSX.
     def test_id_xlsx(self):
         self.assertEqual(
-                file_reader.id_file(self.xlsx_file),
+                fileread.id_file(self.xlsx_file),
                 "XLSX",
                 "Incorrect file type")
 
+    # if the id is an unknown file, it reads as UNKNOWN.
+    def test_id_unknown(self):
+        self.assertEqual(
+                fileread.id_file("asdada"),
+                "UNKNOWN",
+                "Incorrect file type")
 
 # READ CSV FILE TESTS
 
