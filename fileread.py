98102648723b5fb4c03ead6c4157eca9af77c247
Natalija Paunic <natalijapaunic@hotmail.com>
2022-09-28 13:01:30 +0200

    Fixes rebase issues

--- fileread.py
+++ fileread.py
@@ -5,23 +5,10 @@ import pandas as pd
 def read_file(filename):
     filetype = get_filetype(filename)
     if filetype == "CSV":
-<<<<<<< HEAD
-        if not separator:
-            seperator = ";"   
-            """
-            try:
-                pd.read_csv(filename[:, 0], seperator = ",")
-                seperator = ","
-            except:
-                seperator = ";"
-            """
-        return pd.read_csv(filename, sep=separator)
-=======
         try:
             return pd.read_csv(filename, sep = ",")
         except:
             return pd.read_csv(filename, sep = ";")
->>>>>>> 52673b7 (fixed issues on read_file_test)
     elif filetype == "TSV":
         return pd.read_csv(filename, sep='\t')
     elif filetype == "XLS" or filetype == "XLSX":
@@ -38,11 +25,4 @@ def get_filetype(filename):
         return "XLS"
     if filename[-5:] == ".xlsx":
         return "XLSX"
-
-<<<<<<< HEAD
     return "UNKNOWN"
-=======
-    return "UNKNOWN"
-
-#print(read_file("testfiles/helarsprestationer_from_2017.csv"))
->>>>>>> 52673b7 (fixed issues on read_file_test)
