817c5f6fcbc896324aff8db23ab298fcad2b7ed1
Natalija Paunic <natalijapaunic@hotmail.com>
2022-10-01 13:38:00 +0200

    Renames fileread to file_handler

--- test_file_handler.py
+++ test_file_handler.py
@@ -1,6 +1,6 @@
 #!/bin/env python3
 import unittest
-import fileread
+import file_handler
 
 class ReadFileTestCase(unittest.TestCase):
     def setUp(self):
@@ -14,41 +14,41 @@ class ReadFileTestCase(unittest.TestCase):
     # if the id is a csv file, it reads as CSV.
     def test_get_filetype_csv(self):
         self.assertEqual(
-                fileread.get_filetype(self.csv_file),
+                file_handler.get_filetype(self.csv_file),
                 "CSV",
                 "Incorrect file type")
 
     # if the id is a tsv file, it reads as TSV.
     def test_get_filetype_tsv(self):
-        self.assertEqual(fileread.get_filetype(self.tsv_file), 
+        self.assertEqual(file_handler.get_filetype(self.tsv_file), 
                 "TSV",
                 "Incorrect file type")
     
     # if the id isa xls file, it reads as XLS.
     def test_get_filetype_xls(self):
         self.assertEqual(
-                fileread.get_filetype(self.xls_file),
+                file_handler.get_filetype(self.xls_file),
                 "XLS",
                 "Incorrect file type")
 
     # if the file id is a xlsx file, it reads as XLSX.
     def test_get_filetype_xlsx(self):
         self.assertEqual(
-                fileread.get_filetype(self.xlsx_file),
+                file_handler.get_filetype(self.xlsx_file),
                 "XLSX",
                 "Incorrect file type")
 
     # if the id is an unknown file, it reads as UNKNOWN.
     def test_get_filetype_unknown(self):
         self.assertEqual(
-                fileread.get_filetype("asdada"),
+                file_handler.get_filetype("asdada"),
                 "UNKNOWN",
                 "Incorrect file type")
 
     # READ CSV FILE TESTS
     # files with semicolon.
     def test_read_file_csv_semicolon(self):
-        pd = fileread.read_file(self.csv_semicolon_file)
+        pd = file_handler.read_file(self.csv_semicolon_file)
         self.assertTrue(pd is not None)
         # Expect 11 rows and 9 columns
         self.assertEqual(pd.shape, (10, 9))
@@ -62,7 +62,7 @@ class ReadFileTestCase(unittest.TestCase):
         self.assertEqual(list(pd.columns), labels)
     
     def test_read_file_csv(self):
-       pd = fileread.read_file(self.csv_file)
+       pd = file_handler.read_file(self.csv_file)
        self.assertTrue(pd is not None)
        # Expect 11 rows and 9 columns
        self.assertEqual(pd.shape, (10,9))
@@ -78,7 +78,7 @@ class ReadFileTestCase(unittest.TestCase):
 # READ TSV FILE TESTS
     # files with tabs.
     def test_read_file_tsv(self):
-       pd = fileread.read_file(self.tsv_file)
+       pd = file_handler.read_file(self.tsv_file)
        self.assertTrue(pd is not None)
        # Expect 11 rows and 9 columns
        self.assertEqual(pd.shape, (10,9))
@@ -94,7 +94,7 @@ class ReadFileTestCase(unittest.TestCase):
 # READ XLS
     # files with xls.
     def test_read_file_xls(self):
-       pd = fileread.read_file(self.xls_file)
+       pd = file_handler.read_file(self.xls_file)
        self.assertTrue(pd is not None)
        # Expect 11 rows and 9 columns
        self.assertEqual(pd.shape, (10,9))
@@ -110,7 +110,7 @@ class ReadFileTestCase(unittest.TestCase):
 # READ XLSX FILE TEST
     # files with xlsx.
     def test_read_file_xlsx(self):
-        pd = fileread.read_file(self.xlsx_file)
+        pd = file_handler.read_file(self.xlsx_file)
         self.assertTrue(pd is not None)
         # Expect 11 rows and 9 columns
         self.assertEqual(pd.shape, (10,9))
@@ -126,5 +126,5 @@ class ReadFileTestCase(unittest.TestCase):
 # READ BAD FILE
     # files that are incorrect.
     def test_read_file_bad(self):
-        pd = fileread.read_file("ADASDASDASD")
+        pd = file_handler.read_file("ADASDASDASD")
         self.assertTrue(pd is None)
